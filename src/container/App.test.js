e97b8e4b9c95fe83bce40574c28e8c3e54de8d63
Lmanti <luisangelherreramantilla@gmail.com>
2021-07-30 14:45:08 -0500

    unit testing added

--- src/container/App.test.js
+++ src/container/App.test.js
@@ -1,8 +1,33 @@
 import { render, screen } from '@testing-library/react';
+import Players from '../components/Players/Players';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Search Bar', () => {
+
+  beforeEach(() => render(<App />));
+
+  it('must display a button.', () => {
+    const buttonElement = screen.getByRole('button');
+    expect(buttonElement).toBeInTheDocument();
+  })
+  it('must display an input.', () => {
+    const inputElement = screen.getByRole('spinbutton');
+    expect(inputElement).toBeInTheDocument();
+  })
+})
+
+describe('Players', () => {
+  const result = ["Alex Acker - Hassan Adams", "Arron Afflalo - Maurice Ager"]
+
+  it('must display "No matches found" if there\'s no results.', () => {
+    render(<Players result={"Empty"} />)
+    const innerElement = screen.getByText(/No matches found/i);
+    expect(innerElement).toBeInTheDocument();
+  })
+
+  it('must display the information.', () => {
+    render(<Players result={result} />)
+    const innerElement = screen.getByText(/Alex Acker - Hassan Adams/i);
+    expect(innerElement).toBeInTheDocument();
+  })
+})
